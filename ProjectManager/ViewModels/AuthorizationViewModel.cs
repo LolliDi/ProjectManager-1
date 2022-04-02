59ea2f9fbf3fda0ee5ca503e3a0acbd46e55cd51
Saha <t343st@gmail.com>
2022-04-02 15:45:47 +0300

    Сделал стили

---
+++ ProjectManager/ViewModels/AuthorizationViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.ViewModels
+{
+    class AuthorizationViewModel
+    {
+
+        string login, pass;
+        public string GetLogin
+        {
+            set
+            {
+                login = value;
+            }
+        }
+
+        public string GetPass
+        {
+            set
+            {
+                pass = value;
+            }
+        }
+
+
+    }
+}
