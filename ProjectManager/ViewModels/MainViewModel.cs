196835465895ef54c3e84bddbbfd6056ba9e2041
Barsik_Kato <noran-kotov@yandex.ru>
2022-04-02 19:43:30 +0300

    Список проектов

--- ProjectManager/ViewModels/MainViewModel.cs
+++ ProjectManager/ViewModels/MainViewModel.cs
@@ -17,10 +17,13 @@ namespace ProjectManager.ViewModels
             var r = new ProjectManagerContext();
             var f = r.Users.First();
             Repository<Users> repository = new Repository<Users>(new ProjectManagerContext());
+            Repository<Projects> repositoryPr = new Repository<Projects>(new ProjectManagerContext());
             Users user = repository.Get(1);
+            List<Projects> projects = new List<Projects>() { repositoryPr.Get(1) };
             ToViewModel1Commmand = new LambdaCommand(parameter => CurrentViewModel = new ViewModel1());
             ToViewModel2Commmand = new LambdaCommand(parameter => CurrentViewModel = new ViewModel2());
             ToUserAccountViewModel = new LambdaCommand(parameter => CurrentViewModel = new UserAccountViewModel(user));
+            ToProjectMenuViewModel = new LambdaCommand(parameter => CurrentViewModel = new ProjectMenuViewModel(projects));
         }
         private ViewModel currentViewModel;
         public ViewModel CurrentViewModel
@@ -31,5 +34,6 @@ namespace ProjectManager.ViewModels
         public ICommand ToViewModel1Commmand { get; set; }
         public ICommand ToViewModel2Commmand { get; set; }
         public ICommand ToUserAccountViewModel { get; set; }
+        public ICommand ToProjectMenuViewModel { get; set; }
     }
 }
--- ProjectManager/ViewModels/ProjectMenuViewModel.cs
+++ ProjectManager/ViewModels/ProjectMenuViewModel.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjectManager.Models;
 
 namespace ProjectManager.ViewModels
 {
     class ProjectMenuViewModel : ViewModel
     {
+        public ProjectMenuViewModel(List<Projects> projectList)
+        {
+            this.projectList = projectList;
+        }
+
+        private List<Projects> projectList;
+        public List<Projects> ProjectList
+        {
+            get => projectList;
+            set => projectList = value;
+        }
 
     }
 }
