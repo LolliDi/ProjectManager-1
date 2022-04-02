d3d5996310e4b0a93b50aad5c98e92f741d53428
Kalamponus <m.maiorov@inbox.ru>
2022-04-02 15:34:50 +0300

    Создал макет страницы с инфой о пользователе

---
+++ ProjectManager/ViewModels/UserAccountViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using ProjectManager.Models;
+
+namespace ProjectManager.ViewModels
+{
+    class UserAccountViewModel : ViewModel
+    {
+
+        private List<Users> users = new List<Users>();
+        private List<Persons> persons = new List<Persons>();
+        private CollectionViewSource userDataViewSource;
+
+        public ICollectionView UserCollectionView => userDataViewSource.View;
+    }
+}
