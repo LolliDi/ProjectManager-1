83a9a38253e1457674acc5a65b0a12d18e78b5a6
ra5um1st <saintsprizrakk@gmail.com>
2022-04-04 23:22:59 +0300

    Изменены модели представления для проекта

--- ProjectManager/ViewModels/ProjectTasksViewModel.cs
+++ ProjectManager/ViewModels/ProjectTasksViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using ProjectManager.Models;
+using ProjectManager.Models.Repositories;
+using ProjectManager.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +14,20 @@ namespace ProjectManager.ViewModels
         public ProjectTasksViewModel(Projects project)
         {
 
+            projectTaskResourcesRepository = new Repository<ProjectTaskResources>(new ProjectManagerContext());
+            projectTasksRepository = new Repository<ProjectTasks>(new ProjectManagerContext());
+
+            ToResourcePageCommand = new LambdaCommand(parameter => NavigationService.CurrentViewModel = new ProjectResourcesViewModel(NavigationService, project));
+            ToReportsPageCommand = new LambdaCommand(parameter => NavigationService.CurrentViewModel = new ReportsViewModel(NavigationService));
+            ToProjectPassportPageCommand = new LambdaCommand(parameter => NavigationService.CurrentViewModel = new ProjectPassportViewModel(NavigationService));
         }
+
+        private readonly IRepository<ProjectTasks> projectTasksRepository;
+        private readonly IRepository<ProjectTaskResources> projectTaskResourcesRepository;
+
+        public NavigationService NavigationService { get; set; }
+        public ICommand ToResourcePageCommand { get; set; }
+        public ICommand ToReportsPageCommand { get; set; }
+        public ICommand ToProjectPassportPageCommand { get; set; }
     }
 }
