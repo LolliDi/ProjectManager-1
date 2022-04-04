ba14d40971382c33fe06c7072547eb495a4a3477
Barsik_Kato <noran-kotov@yandex.ru>
2022-04-04 12:18:08 +0300

    Вывод информации о выбранном проекте

--- ProjectManager/ViewModels/ProjectMenuViewModel.cs
+++ ProjectManager/ViewModels/ProjectMenuViewModel.cs
@@ -9,6 +9,7 @@ namespace ProjectManager.ViewModels
 {
     class ProjectMenuViewModel : ViewModel
     {
+        private int selectedId = 0;
         public ProjectMenuViewModel(List<Projects> projectList)
         {
             this.projectList = projectList;
@@ -19,6 +20,35 @@ namespace ProjectManager.ViewModels
         {
             get => projectList;
             set => projectList = value;
+        }
+
+        public int SelectedProject
+        {
+            get => selectedId;
+            set => selectedId = value;
+        }
+
+        public string SelectedProjectTitle
+        {
+            get => projectList[selectedId].Name;
+        }
+
+        public string SelectedProjectDate
+        {
+            get => projectList[selectedId].CreatingDate.ToString();
+        }
+
+        public string SelectedProjectUsers
+        {
+            get
+            {
+                string userList = "Список пользователей: ";
+                foreach(Users user in projectList[selectedId].Users)
+                {
+                    userList += "\n" + user.Username;
+                }
+                return userList;
+            }
         }
 
     }
