b3e9a7cf3c0f22b8d85a4906c11ff79408f97f61
ra5um1st <saintsprizrakk@gmail.com>
2022-04-04 16:42:32 +0300

    Изменено название ProjectMenuPage на TasksMenuPage и создана TasksViewModel

---
+++ ProjectManager/ViewModels/TasksMenuViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ProjectManager.Commands;
+using ProjectManager.Models;
+using ProjectManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ProjectManager.ViewModels
+{
+    class TasksMenuViewModel : ViewModel
+    {
+        public TasksMenuViewModel(NavigationService navigationService, Projects project)
+        {
+            NavigationService = navigationService;
+
+            ToResourcePageCommand = new LambdaCommand(null);
+            ToReportsPageCommand = new LambdaCommand(null);
+            ToGanttChartPageCommand = new LambdaCommand(null);
+            ToProjectPassportPageCommand = new LambdaCommand(null);
+        }
+
+        public NavigationService NavigationService { get; set; }
+        public ICommand ToResourcePageCommand { get; set; }
+        public ICommand ToReportsPageCommand { get; set; }
+        public ICommand ToGanttChartPageCommand { get; set; }
+        public ICommand ToProjectPassportPageCommand { get; set; }
+    }
+}
