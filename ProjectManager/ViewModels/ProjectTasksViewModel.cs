bf97f20bc32778782af3ada4cec48514303ffb42
ra5um1st <saintsprizrakk@gmail.com>
2022-04-05 18:21:03 +0300

    Добавил модель представления для задач проекта, изменил название для модели представления меню проекта.

--- ProjectManager/ViewModels/ProjectTasksViewModel.cs
+++ ProjectManager/ViewModels/ProjectTasksViewModel.cs
@@ -1,31 +1,17 @@
-﻿using ProjectManager.Commands;
-using ProjectManager.Models;
-using ProjectManager.Services;
+﻿using ProjectManager.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Input;
 
 namespace ProjectManager.ViewModels
 {
     class ProjectTasksViewModel : ViewModel
     {
-        public ProjectTasksViewModel(NavigationService navigationService, Projects project)
+        public ProjectTasksViewModel(Projects project)
         {
-            NavigationService = navigationService;
 
-            ToResourcePageCommand = new LambdaCommand(null);
-            ToReportsPageCommand = new LambdaCommand(null);
-            ToGanttChartPageCommand = new LambdaCommand(null);
-            ToProjectPassportPageCommand = new LambdaCommand(null);
         }
-
-        public NavigationService NavigationService { get; set; }
-        public ICommand ToResourcePageCommand { get; set; }
-        public ICommand ToReportsPageCommand { get; set; }
-        public ICommand ToGanttChartPageCommand { get; set; }
-        public ICommand ToProjectPassportPageCommand { get; set; }
     }
 }
