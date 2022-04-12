c78787b9837d4b65891e9eaabdb5c82a55ac5eb7
Kalamponus <m.maiorov@inbox.ru>
2022-04-12 14:48:08 +0300

    Добавил функцию изменения данных о текущем пользователе

--- ProjectManager/ViewModels/UserAccountViewModel.cs
+++ ProjectManager/ViewModels/UserAccountViewModel.cs
@@ -16,13 +16,44 @@ namespace ProjectManager.ViewModels
     class UserAccountViewModel : ViewModel
     {
         private Users user;
+        private string name = "", surname = "", patronymic = "";
+        private Nullable<int> age;
         NavigationService NavigationService;
         public ICommand Back { get; set; }
+        public ICommand Redact { get; set; }
         public UserAccountViewModel(Users user, NavigationService NavigationService)
         {
             this.user = user;
             this.NavigationService = NavigationService;
+            if(user != null && user.Persons != null)
+            {
+                name = user.Persons.Name;
+                surname = user.Persons.Surname;
+                patronymic = user.Persons.Patronymic;
+                age = user.Persons.Age;
+            }            
             Back = new LambdaCommand(parameter => NavigationService.CurrentViewModel = new ProjectListViewModel(NavigationService,user));
+            Redact = new LambdaCommand(RedactUser);
+        }       
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+        public string Patronymic
+        {
+            get { return patronymic; }
+            set { patronymic = value; }
+        }
+        public Nullable<int> Age
+        {
+            get { return age; }
+            set { age = value; }
         }
         public Users User
         {
@@ -35,6 +66,24 @@ namespace ProjectManager.ViewModels
             {
                 this.user = value;
             }
+        }
+        private void RedactUser(object obj)
+        {
+            Repository<Persons> repository = new Repository<Persons>();
+            UsersRepository usersRepository = new UsersRepository();
+            if (user.Persons != null)
+            {
+                user.Persons.Name = Name;
+                user.Persons.Surname = Surname;
+                user.Persons.Patronymic = Patronymic;
+                user.Persons.Age = Age;
+            }
+            else
+            {
+                Persons person = new Persons() { Age = this.Age, Name = this.Name, Patronymic = this.Patronymic, Surname = this.Surname, Id = user.Id};               
+                repository.Add(person);
+            }                                    
+            usersRepository.Update(user);
         }
     }
 }
