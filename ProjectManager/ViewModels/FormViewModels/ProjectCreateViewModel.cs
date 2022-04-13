fb04f50705e949406103afa4ca8f0bf39fbbe507
Saha <t343st@gmail.com>
2022-04-13 23:06:30 +0300

    Начало создания интерфейса для CreateProjectView

---
+++ ProjectManager/ViewModels/FormViewModels/ProjectCreateViewModel.cs
@@ -0,0 +1,104 @@
+﻿using ProjectManager.Commands;
+using ProjectManager.Models;
+using ProjectManager.Models.Repositories;
+using ProjectManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ProjectManager.ViewModels
+{
+    class ProjectCreateViewModel : FormViewModel
+    {
+
+        private NavigationService NavigationService;
+        private Users currentUser;
+        private Repository<Users> usersRepository;
+
+
+        private SolidColorBrush standartColor = new SolidColorBrush(Color.FromRgb(250, 250, 250));
+
+        private string title = "";
+        private SolidColorBrush titleBorderColor;
+        private List<Users> usersList;
+
+        public ICommand ToNewProject { get; set; }
+        public ICommand ToBack { get; set; }
+        public ProjectCreateViewModel(FormNavigationService navigationService, Users currentUser) : base(navigationService)
+        {
+            this.currentUser = currentUser;
+            NavigationService = navigationService;
+
+            usersRepository = new Repository<Users>();
+
+            titleBorderColor = standartColor;
+            UsersList = new List<Users>();
+
+            ToNewProject = new LambdaCommand(GoNewProject);
+            ToBack = new LambdaCommand(GoBack);
+        }
+       
+        public string Title
+        {
+            get => title;
+            set => Set(ref title, ref value);
+        }
+
+        public SolidColorBrush TitleBorderColor
+        {
+            get => titleBorderColor;
+            set => Set(ref titleBorderColor, ref value);
+        }
+
+        public List<Users> UsersList
+        {
+            get => usersList;
+            set
+            {
+                List<Users> users = value;
+                foreach (Users user in usersRepository.Items)
+                {
+                    if (user.Username != currentUser.Username && user.Roles.Permissions.Where(x => x.Name == "permission_privateprojects").ToList().Any())
+                    {
+                        users.Add(user);
+                    }
+                }
+                usersList = users;
+            }
+        }
+
+        private void GoNewProject(object obj)
+        {
+            Projects newProject = new Projects();
+            if (title.Length < 1 || title.Length > 50)
+                return;
+            newProject.Name = title;
+            newProject.CreatingDate = DateTime.Now;
+            newProject.User = currentUser.Id;
+            newProject.Users.Add(usersRepository.Items.Where(x => x.Id == currentUser.Id).FirstOrDefault()); //Приходится делать так, потому что разные контексты
+            var selectedUsers = ((IEnumerable<object>)obj).Cast<object>().ToList();
+            if (selectedUsers.Count > 0)
+            {
+                foreach (var item in selectedUsers)
+                {
+                    newProject.Users.Add((Users)item);
+                }
+            }
+            ProjectsRepository projectsRepository = new ProjectsRepository();
+            projectsRepository.Add(newProject);
+            NavigationService.CurrentViewModel = new ProjectListViewModel(NavigationService, currentUser);
+        }
+        private void GoBack(object obj)
+        {
+            NavigationService.CurrentViewModel = new ProjectListViewModel(NavigationService, currentUser);
+        }
+
+    }
+}
