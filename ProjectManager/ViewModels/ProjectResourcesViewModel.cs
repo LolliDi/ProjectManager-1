ae4858091fe66331776768d70f2a08ef654193be
Saha <t343st@gmail.com>
2022-04-14 18:45:55 +0300

    Измененние интерфейса в ResourcesPage и UsreViewPage

--- ProjectManager/ViewModels/ProjectResourcesViewModel.cs
+++ ProjectManager/ViewModels/ProjectResourcesViewModel.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using ProjectManager.Models.Repositories;
 using System.Windows.Input;
 using ProjectManager.Commands;
+using System.Windows;
 
 namespace ProjectManager.ViewModels
 {
@@ -26,15 +27,30 @@ namespace ProjectManager.ViewModels
             foreach (var item in projectResourcesRepository.Items.Where(x => x.Project == currentProject.Id))
                 resources.Add(item.Resources);
             ToProjectResourcesCreateViewModel = new LambdaCommand(ToProjectResourcesCreateViewModelCommandExecute);
+            ToProjectResourcesEditViewModel = new LambdaCommand(ToProjectResourcesEditViewModelCommandExecute);
+            ToProjectResourcesDeleteViewModel = new LambdaCommand(ToProjectResourcesDeleteViewModelCommandExecute);
         }
         public List<Resources> Resources
         {
             get { return resources; }
         }
-        public ICommand ToProjectResourcesCreateViewModel { get; set; }        
+        public ICommand ToProjectResourcesCreateViewModel { get; set; }
+        public ICommand ToProjectResourcesEditViewModel { get; set; }
+        public ICommand ToProjectResourcesDeleteViewModel { get; set; }
+
         private void ToProjectResourcesCreateViewModelCommandExecute(object parameter)
         {
             navigationService.CurrentViewModel = new ProjectResourcesCreateViewModel(navigationService, currentProject);
+        }
+
+        private void ToProjectResourcesEditViewModelCommandExecute(object parameter)
+        {
+            MessageBox.Show("В разработке", "Инфомация", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void ToProjectResourcesDeleteViewModelCommandExecute(object parameter)
+        {
+            MessageBox.Show("В разработке", "Инфомация", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
