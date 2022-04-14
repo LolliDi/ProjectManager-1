f403e9bf354eed401709dfed7a28235b0e7482d6
Barsik_Kato <noran-kotov@yandex.ru>
2022-04-14 23:56:26 +0300

    Еще чуть-чуть

--- ProjectManager/ViewModels/MainViewModel.cs
+++ ProjectManager/ViewModels/MainViewModel.cs
@@ -15,31 +15,10 @@ namespace ProjectManager.ViewModels
     {
         public MainViewModel()
         {
-            usersRepository = new Repository<Users>();
-            projectsRepository = new Repository<Projects>();
             NavigationService = new NavigationService();
             NavigationService.CurrentViewModel = new AuthorizationViewModel(NavigationService);
-
-            Users user = usersRepository.Get(1);
-            Projects project = projectsRepository.Get(1);
-
-            ToUserAccountViewModel = new LambdaCommand(parameter => NavigationService.CurrentViewModel = new UserAccountViewModel(user, NavigationService));
-            ToProjectListViewModel = new LambdaCommand(parameter => NavigationService.CurrentViewModel = new ProjectListViewModel(NavigationService, user));
-            ToViewModelAuto = new LambdaCommand(parameter => NavigationService.CurrentViewModel = new AuthorizationViewModel(NavigationService));
-            ToAddUserPage = new LambdaCommand(parameter => NavigationService.CurrentViewModel = new AddUserPageViewModel(NavigationService));
-            ToUsersViewPage = new LambdaCommand(parameter => NavigationService.CurrentViewModel = new UsersViewPageViewModel(NavigationService));
-            ToProjectMenuViewModel = new LambdaCommand(parameter => NavigationService.CurrentViewModel = new ProjectMenuViewModel(NavigationService, project, user));
         }
 
-        private readonly IRepository<Users> usersRepository;
-        private readonly IRepository<Projects> projectsRepository;
-
         public NavigationService NavigationService { get; set; }
-        public ICommand ToUserAccountViewModel { get; set; }
-        public ICommand ToProjectListViewModel { get; set; }
-        public ICommand ToViewModelAuto { get; set; }
-        public ICommand ToAddUserPage { get; set; }
-        public ICommand ToUsersViewPage { get; set; }
-        public ICommand ToProjectMenuViewModel { get; set; }
     }
 }
